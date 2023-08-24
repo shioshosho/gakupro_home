d5cf393c2e47a3c722d4b221046e396525c0fb4e
shioshosho <nezaki.sho102@mail.kyutech.jp>
2023-08-24 23:29:23 +0900

    add: Location Bullet (Menu)

---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next'
+//import Test from '@/components/Test'
+import Menu from '@/components/Menu'
+
+const TopPage: NextPage = () => {
+  return (
+    <>
+      {/*
+        *<Test test='hoge' />
+        */}
+      <Menu />
+    </>
+  )
+}
+
+export default TopPage
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,17 +1,44 @@
+'use client'
 import { FC } from 'react'
+import { usePathname } from 'next/navigation'
 
 type Props = {
   //text :string
 }
 
 const Menu: FC<Props> = () => {
+  const location = usePathname()
   return(
     <>
-      <div className="flex divide-x justify-center p-5">
-        <a href="localhost:3000" className="px-4 py-1">Home</a>
-        <a href="localhost:3000/About" className="px-4 py-1">About</a>
-        <a href="localhost:3000/News" className="px-4 py-1">News</a>
-        <a href="localhost:3000/Contact" className="px-4 py-1">Contact</a>
+      <div className="flex divide-x divide-black justify-center p-5">
+        <div className="flex flex-col items-center">
+          <a href="/" className="px-4 py-1">Home</a>
+          {location==='/'
+            ? <svg viewBox='0 0 2 2' width='5px'><circle cx="1" cy="1" r="1"/></svg>
+            : null
+          }
+        </div>
+        <div className="flex flex-col items-center">
+          <a href="/About" className="px-4 py-1">About</a>
+          {location==='/About'
+            ? <svg viewBox='0 0 2 2' width='5px'><circle cx="1" cy="1" r="1"/></svg>
+            : null
+          }
+        </div>
+        <div className="flex flex-col items-center">
+          <a href="/News" className="px-4 py-1">News</a>
+          {location==='/News'
+            ? <svg viewBox='0 0 2 2' width='5px'><circle cx="1" cy="1" r="1"/></svg>
+            : null
+          }
+        </div>
+        <div className="flex flex-col items-center">
+          <a href="/Contact" className="px-4 py-1">Contact</a>
+          {location==='/Contact'
+            ? <svg viewBox='0 0 2 2' width='5px'><circle cx="1" cy="1" r="1"/></svg>
+            : null
+          }
+        </div>
       </div>
     </>
   )
